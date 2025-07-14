8cf98026f27f5ec3363438c4435c0422e75bf01f
j3cohen <joshua3Cohen@gmail.com>
2025-07-14 15:59:13 -0400

    hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,8 @@ export default function Hero() {
         style={{
           backgroundImage:
             // "url('https://images.unsplash.com/photo-1554068865-24cecd4e34b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80')",
-            "url('https://jbcpublicbucket.s3.us-east-1.amazonaws.com/hero.JPG')",
+            //"url('https://jbcpublicbucket.s3.us-east-1.amazonaws.com/hero.JPG')",
+            "url('https://jbcpublicbucket.s3.us-east-1.amazonaws.com/website-content/josh-busy-skyball.jpeg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           transform: `translateY(${offset * 0.5}px)`,
